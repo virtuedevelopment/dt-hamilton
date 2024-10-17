ba69e21ed0b00e5f9967f37e6aebf8d3b0ff512a
Virtue Engine <101955083+virtuedevelopment@users.noreply.github.com>
2024-10-17 18:34:58 -0400

    added services page

--- app/(routes)/about/page.jsx
+++ app/(routes)/about/page.jsx
@@ -13,24 +13,45 @@ export default function page() {
 
       <section className={styles.bio}>
         <div className={styles.info}>
-          <h2>What we offer</h2>
+          <h2>What We Do</h2>
           <p>
-            At DT Hamilton HVAC, we are dedicated to providing expert heating,
-            ventilation, and air conditioning solutions for both residential and
-            commercial spaces. Our team of skilled professionals offers a full
-            range of services, ensuring your environment remains comfortable,
-            energy-efficient, and reliable year-round.
+            At DT Hamilton HVAC, we are committed to delivering top-tier
+            heating, ventilation, and air conditioning solutions tailored to
+            meet the unique needs of both residential and commercial spaces.
+            Whether you are a homeowner seeking cozy warmth during winter or a
+            business owner needing efficient cooling for a bustling office, our
+            comprehensive range of services ensures that your space stays
+            comfortable and functional in every season.
             <br />
+            <br />
+            Our team of skilled professionals brings extensive expertise,
+            performing everything from routine maintenance and emergency repairs
+            to complete system installations and upgrades. We work with
+            cutting-edge technologies and high-efficiency systems designed to
+            improve air quality, lower energy consumption, and enhance
+            operational reliability.
+            <br />
+            <br />
+            We understand that every environment has distinct demands, so we
+            take a customized approach to each project, ensuring all
+            installations and repairs are optimized for maximum performance. At
+            DT Hamilton HVAC, client satisfaction is our top priority, and we
+            pride ourselves on delivering exceptional service and dependable
+            solutions that exceed expectations.
+            <br />
+            <br />
+            Whether you need seasonal maintenance, ductwork design,
+            construction, or a new HVAC installation, our expert team is ready
+            to ensure your system runs smoothly and cost-effectively year-round.
+            Let us partner with you to create a comfortable, energy-efficient
+            space that you can rely on.
           </p>
           <Link href={"/"} className="button-main">
             Our Services
           </Link>
         </div>
         <div className={styles.image}>
-          <img
-            src="https://images.pexels.com/photos/14982942/pexels-photo-14982942/free-photo-of-trees-seen-from-roof-of-building.jpeg?auto=compress&cs=tinysrgb&w=800"
-            alt="Image of roof with A/C"
-          />
+          <img src="/images/about-img-02.jpeg" alt="Image of roof with A/C" />
         </div>
       </section>
 
@@ -54,6 +75,19 @@ export default function page() {
           <LeafyGreen />
           <p>Eco-Friendly Solutions</p>
         </span>
+      </section>
+
+      <section className={styles.call_to_action}>
+        <p>
+          Ready to elevate your comfort? Contact DT Hamilton HVAC today for
+          expert solutions that keep your space efficient, reliable, and
+          comfortable all year round. Whether it's maintenance, repairs, or a
+          brand-new installation, our team is here to help. Book your
+          consultation now and experience the difference!
+        </p>
+        <Link href={"/getQuote"} className="button-white">
+          Get Quote
+        </Link>
       </section>
     </>
   );
--- app/(routes)/services/page.jsx
+++ app/(routes)/services/page.jsx
@@ -1,5 +1,21 @@
 import React from "react";
+import styles from "./services.module.css";
+import configurations from "@/_data/config";
+import ServiceCard from "@/app/components/Cards/ServiceCard";
 
 export default function page() {
-  return <div>services page</div>;
+  return (
+    <>
+      <header className={styles.header}>
+        <h1>Our Services</h1>
+        <h6>Learn more about how we can help you</h6>
+      </header>
+
+      <section className={styles.services}>
+        {configurations.services.map((service, index) => (
+          <ServiceCard key={index} card={service} />
+        ))}
+      </section>
+    </>
+  );
 }
